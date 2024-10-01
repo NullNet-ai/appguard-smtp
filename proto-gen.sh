746c0b37c211ef9dd22b74cccc3422214e417a98
GyulyVGC <gyulyvgc99@gmail.com>
2024-10-01 17:38:17 +0200

    fix build scripts

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf src/proto
-./node_modules/.bin/proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=src/proto/ ./proto/appguard.proto
+./node_modules/.bin/proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=src/proto/ ./appguard-protobuf/appguard.proto
